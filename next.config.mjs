9caf6491d9b4c5a506df0259f0d4de3b797656aa
Ronak Prajapati <ronakprajapati3545@gmail.com>
2026-05-01 23:10:04 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -7,6 +8,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: '/tour-and-travel-web',
+  assetPrefix: '/tour-and-travel-web/',
+
 }
 
 export default nextConfig
